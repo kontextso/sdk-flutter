0436ffbe17a5019eaee118a7ba1d9acd00f72337
Duc Phi Viet <duc.phiviet01@gmail.com>
2025-07-22 14:47:51 +0200

    Enhance inline ad component with iframe visibility and resizing features

--- lib/src/widgets/inline_ad.dart
+++ lib/src/widgets/inline_ad.dart
@@ -45,68 +45,82 @@ class InlineAd extends HookWidget {
     }
 
     final iframeLoaded = useState(false);
+    final showIframe = useState(false);
+    final height = useState(.0);
 
     final messages = adsProviderData.messages;
 
+    return Offstage(
+      offstage: !iframeLoaded.value || !showIframe.value,
+      child: SizedBox(
+        height: height.value,
+        width: double.infinity,
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(
+            url: WebUri('https://server.develop.megabrain.co/api/frame/${bid.id}?code=$code&messageId=$messageId'),
+          ),
+          initialSettings: InAppWebViewSettings(
+            useShouldOverrideUrlLoading: true,
+          ),
+          shouldOverrideUrlLoading: (controller, navigationAction) async {
+            final url = navigationAction.request.url?.toString();
+            print('Navigating to URL: $url');
 
-    return SizedBox(
-      height: 100,
-      child: InAppWebView(
-        initialUrlRequest: URLRequest(
-          url: WebUri('https://server.develop.megabrain.co/api/frame/${bid.id}?code=$code&messageId=$messageId'),
-        ),
-        initialSettings: InAppWebViewSettings(
-          useShouldOverrideUrlLoading: true,
-        ),
-        shouldOverrideUrlLoading: (controller, navigationAction) async {
-          final url = navigationAction.request.url?.toString();
-          print('Navigating to URL: $url');
-
-          return NavigationActionPolicy.CANCEL;
-        },
-        onConsoleMessage: (controller, consoleMessage) {
-          print('Console Message: ${consoleMessage.message}');
-        },
-        onWebViewCreated: (controller) {
-          controller.addJavaScriptHandler(
-            handlerName: 'message',
-            callback: (args) {
-              print('Message from iframe: $args');
-              final message = args.firstOrNull;
-              if (message == null || message is! Json) {
-                return;
-              }
-
-              switch (message['type']) {
-                case 'init-iframe':
-                  print('Initializing iframe with message: $message');
-                  iframeLoaded.value = true;
-                  _postUpdateIframe(controller, messages: messages);
-                  break;
-                default:
-                  print('Unknown message type: ${message['type']}');
-              }
-              print('Received message: $message');
-            },
-          );
-        },
-        onReceivedError: (controller, request, error) {
-          print('onReceivedError: $error');
-        },
-        onReceivedHttpError: (controller, request, error) {
-          print('onReceivedHttpError: $error');
-        },
-        onLoadStop: (controller, url) async {
-          await controller.evaluateJavascript(source: '''
-                console.log('InAppWebView loaded with URL: $url');
-                if (!window.__flutterSdkBridgeReady) {
-                  window.__flutterSdkBridgeReady = true;
-                  window.addEventListener('message', event => {
-                    window.flutter_inappwebview.callHandler('message', event.data);
-                  });
+            return NavigationActionPolicy.CANCEL;
+          },
+          onConsoleMessage: (controller, consoleMessage) {
+            print('Console Message: ${consoleMessage.message}');
+          },
+          onWebViewCreated: (controller) {
+            controller.addJavaScriptHandler(
+              handlerName: 'message',
+              callback: (args) {
+                final message = args.firstOrNull;
+                if (message == null || message is! Json) {
+                  return;
                 }
-              ''');
-        },
+
+                switch (message['type']) {
+                  case 'init-iframe':
+                    print('Initializing iframe with message: $message');
+                    iframeLoaded.value = true;
+                    _postUpdateIframe(controller, messages: messages);
+                    break;
+                  case 'show-iframe':
+                    print('Showing iframe with message: $message');
+                    showIframe.value = true;
+                    break;
+                  case 'resize-iframe':
+                    print('Resizing iframe with message: $message');
+                    final dataHeight = message['data']['height'];
+                    if (dataHeight is num) {
+                      height.value = dataHeight.toDouble();
+                    }
+                    break;
+                  default:
+                    print('Unknown message type: ${message['type']}, message: $message');
+                }
+              },
+            );
+          },
+          onReceivedError: (controller, request, error) {
+            print('onReceivedError: $error');
+          },
+          onReceivedHttpError: (controller, request, error) {
+            print('onReceivedHttpError: $error');
+          },
+          onLoadStop: (controller, url) async {
+            await controller.evaluateJavascript(source: '''
+                  console.log('InAppWebView loaded with URL: $url');
+                  if (!window.__flutterSdkBridgeReady) {
+                    window.__flutterSdkBridgeReady = true;
+                    window.addEventListener('message', event => {
+                      window.flutter_inappwebview.callHandler('message', event.data);
+                    });
+                  }
+                ''');
+          },
+        ),
       ),
     );
   }
