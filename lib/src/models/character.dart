cc251067a0d0e6b97f6e426e464bc55869fc65cc
Duc Phi Viet <duc.phiviet01@gmail.com>
2025-08-31 11:55:59 +0200

    Refactor Character class to require id and streamline JSON serialization

--- lib/src/models/character.dart
+++ lib/src/models/character.dart
@@ -1,10 +1,8 @@
-
 /// The character object used in a conversation.
 class Character {
   Character({
+    required this.id,
     required this.name,
-    this.id,
-    this.title,
     this.greeting,
     this.persona,
     this.tags,
@@ -13,14 +11,17 @@ class Character {
     this.additionalProperties,
   });
 
+  /// Unique identifier for the character.
+  final String id;
+
   /// Name of the character.
   final String name;
 
-  /// Unique identifier for the character.
-  final String? id;
+  /// URL of the character's avatar image.
+  final String? avatarUrl;
 
-  /// Title of the character.
-  final String? title;
+  /// Whether the character is NSFW (Not Safe For Work).
+  final bool? isNsfw;
 
   /// A greeting message from the character.
   final String? greeting;
@@ -31,31 +32,24 @@ class Character {
   /// Tags associated with the character.
   final List<String>? tags;
 
-  /// URL of the character's avatar image.
-  final String? avatarUrl;
-
-  /// Whether the character is NSFW (Not Safe For Work).
-  final bool? isNsfw;
-
   /// Additional properties that can be added to the character.
   final Map<String, dynamic>? additionalProperties;
 
   Map<String, dynamic> toJson() {
     return {
-      'name': name,
       'id': id,
-      'title': title,
-      'greeting': greeting,
-      'persona': persona,
-      'tags': tags,
-      'avatarUrl': avatarUrl,
-      'isNsfw': isNsfw,
+      'name': name,
+      if (avatarUrl != null) 'avatarUrl': avatarUrl,
+      if (isNsfw != null) 'isNsfw': isNsfw,
+      if (greeting != null) 'greeting': greeting,
+      if (persona != null) 'persona': persona,
+      if (tags != null) 'tags': tags,
       if (additionalProperties != null) ...additionalProperties!,
     };
   }
 
   @override
   String toString() {
-    return 'Character(name: $name, id: $id, title: $title, greeting: $greeting, persona: $persona, tags: $tags, avatarUrl: $avatarUrl, isNsfw: $isNsfw, additionalProperties: $additionalProperties)';
+    return 'Character(name: $name, id: $id, greeting: $greeting, persona: $persona, tags: $tags, avatarUrl: $avatarUrl, isNsfw: $isNsfw, additionalProperties: $additionalProperties)';
   }
 }
