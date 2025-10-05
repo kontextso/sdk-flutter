587bbf28a3fe873271051f017cb41be56f9b038e
Duc Phi Viet <duc.phiviet01@gmail.com>
2025-10-05 14:40:36 +0200

    SKOverlay implementation

--- lib/src/utils/types.dart
+++ lib/src/utils/types.dart
@@ -2,3 +2,5 @@ import 'package:kontext_flutter_sdk/src/models/ad_event.dart';
 
 typedef OnEventCallback = void Function(AdEvent event);
 typedef Json = Map<String, dynamic>;
+
+enum OpenIframeComponent { modal, skoverlay }
