e20204460f4b43c63dea81ff65835f4c5cd89716
JakubMrozek <jakub.mrozek@gmail.com>
2025-11-05 19:05:07 +0100

    remove dart_hook_test

---
+++ test/src/widgets/utils/use_preload_ads_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kontext_flutter_sdk/src/widgets/utils/use_preload_ads.dart';
+
+void main() {
+  testWidgets('runs usePreloadAds without crashing', (tester) async {
+
+    expect(true, isTrue);
+  });
+}
