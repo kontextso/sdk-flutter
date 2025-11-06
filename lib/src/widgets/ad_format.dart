c6a55114a6edb26cfab2e5e0743e430833c1ac26
Duc Phi Viet <duc.phiviet01@gmail.com>
2025-11-06 13:35:06 +0100

    Ignore unrecognizable component (#32)

--- lib/src/widgets/ad_format.dart
+++ lib/src/widgets/ad_format.dart
@@ -10,7 +10,7 @@ import 'package:kontext_flutter_sdk/src/services/logger.dart';
 import 'package:kontext_flutter_sdk/src/utils/constants.dart';
 import 'package:kontext_flutter_sdk/src/utils/extensions.dart';
 import 'package:kontext_flutter_sdk/src/utils/kontext_url_builder.dart';
-import 'package:kontext_flutter_sdk/src/utils/types.dart' show OnEventCallback, Json;
+import 'package:kontext_flutter_sdk/src/utils/types.dart' show OnEventCallback, Json, OpenIframeComponent;
 import 'package:kontext_flutter_sdk/src/widgets/ads_provider_data.dart';
 import 'package:kontext_flutter_sdk/src/widgets/utils/select_bid.dart';
 import 'package:kontext_flutter_sdk/src/widgets/interstitial_modal.dart';
@@ -191,18 +191,49 @@ class AdFormat extends HookWidget {
         }
         break;
       case 'open-component-iframe':
-        final component = data?['component'];
-        if (component is! String || component.isEmpty) {
-          Logger.error('Ad component is missing or invalid. Data: $data');
+        final component = OpenIframeComponent.fromValue(data?['component']);
+        if (component == null) {
           return;
         }
 
-        final milliseconds = data?['timeout'];
-        final timeout = (milliseconds is int && milliseconds > 0)
-            ? Duration(milliseconds: milliseconds)
-            : const Duration(seconds: 5);
+        _handleOpenComponentIframe(
+          context,
+          adServerUrl: adServerUrl,
+          inlineUri: inlineUri,
+          bidId: bidId,
+          component: component,
+          data: data,
+          onEvent: adsProviderData.onEvent,
+        );
+        break;
+      case 'error-iframe':
+        resetIframe();
+        break;
+      default:
+    }
+  }
 
-        final modalUri = inlineUri.replacePath('/api/$component/$bidId');
+  void _handleOpenComponentIframe(
+    BuildContext context, {
+    required String adServerUrl,
+    required Uri inlineUri,
+    required String bidId,
+    required OpenIframeComponent component,
+    Json? data,
+    OnEventCallback? onEvent,
+  }) {
+    if (data == null) {
+      Logger.error('Ad component data is missing. Component: $component');
+      return;
+    }
+
+    final milliseconds = data['timeout'];
+    final timeout =
+        (milliseconds is int && milliseconds > 0) ? Duration(milliseconds: milliseconds) : const Duration(seconds: 5);
+
+    switch (component) {
+      case OpenIframeComponent.modal:
+        final modalUri = inlineUri.replacePath('/api/${component.name}/$bidId');
         InterstitialModal.show(
           context,
           adServerUrl: adServerUrl,
@@ -210,15 +241,11 @@ class AdFormat extends HookWidget {
           initTimeout: timeout,
           onEventIframe: (data) => _handleEventIframe(
             adServerUrl: adServerUrl,
-            onEvent: adsProviderData.onEvent,
+            onEvent: onEvent,
             data: data,
           ),
         );
         break;
-      case 'error-iframe':
-        resetIframe();
-        break;
-      default:
     }
   }
 
