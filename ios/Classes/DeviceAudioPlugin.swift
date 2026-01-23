8817f16a889da648f4561e20fb47dcc38f25e857
JakubMrozek <jakub.mrozek@gmail.com>
2026-01-23 20:36:45 +0000

    remove old code

--- ios/Classes/DeviceAudioPlugin.swift
+++ ios/Classes/DeviceAudioPlugin.swift
@@ -20,43 +20,6 @@ public class DeviceAudioPlugin: NSObject, FlutterPlugin {
         }
     }
 
-    /*
-    // Runs a closure while the audio session is briefly active.
-    private func withActivatedSession<T>(_ body: @escaping (AVAudioSession) -> T) -> T {
-        let session = AVAudioSession.sharedInstance()
-
-        let run: () -> T = {
-            let prevCategory = session.category
-            let prevMode = session.mode
-            let prevPolicy = session.routeSharingPolicy
-            let prevOptions = session.categoryOptions
-
-            var didActivate = false
-            do {
-                try session.setActive(true, options: [])
-                didActivate = true
-            } catch {/** Ignore **/}
-
-            let out = body(session)
-
-            if didActivate {
-                _ = try? session.setActive(false, options: [.notifyOthersOnDeactivation])
-            }
-            _ = try? session.setCategory(
-                prevCategory,
-                mode: prevMode,
-                policy: prevPolicy,
-                options: prevOptions
-            )
-
-            return out
-        }
-
-        if Thread.isMainThread { return run() }
-        return DispatchQueue.main.sync(execute: run)
-    }
-    */
-
     private func readAudioInfo() -> [String: Any] {
         let session = AVAudioSession.sharedInstance()
 
